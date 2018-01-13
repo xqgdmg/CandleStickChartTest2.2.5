636c24da49f274a7f27fc3cb1b8f010bc9b24107
Chris <395932265@qq.com>
2018-01-13 21:14:08 +0800

    code

---
+++ app/src/main/java/com/example/administrator/candlestickcharttest225/DemoBase.java
@@ -0,0 +1,19 @@
+
+package com.example.administrator.candlestickcharttest225;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Baseclass of all Activities of the Demo Application.
+ * 
+ * @author Philipp Jahoda
+ */
+public abstract class DemoBase extends FragmentActivity {
+
+
+//    @Override
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//        overridePendingTransition(R.anim.move_left_in_activity, R.anim.move_right_out_activity);
+//    }
+}
